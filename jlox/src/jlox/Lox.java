0e5a0084c81ae6fcbbd9d4603919533e56adde3d
justin <justinac.1999@gmail.com>
2023-12-20 15:28:37 +1000

    Parsing and Evaulating Expressions.

--- jlox/src/jlox/Lox.java
+++ jlox/src/jlox/Lox.java
@@ -9,11 +9,17 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+	private static final Interpreter interpreter = new Interpreter();
+
 	static boolean hadError = false;
+	static boolean hadRuntimeError = false;
 
 	private static void runFile(String path) throws IOException {
 		byte[] bytes = Files.readAllBytes(Paths.get(path));
 		run(new String(bytes, Charset.defaultCharset()));
+
+		if (hadError) System.exit(65);
+		if (hadRuntimeError) System.exit(70);
 	}
 
 	private static void runPrompt() throws IOException {
@@ -23,28 +29,39 @@ public class Lox {
 		for (;;) {
 			System.out.print("> ");
 			String line = reader.readLine();
-			if (line == null)
+
+			if (line == null) {
 				break;
+			}
+
 			run(line);
 			hadError = false;
 		}
 	}
 
 	private static void run(String source) {
-		if (hadError)
-			System.exit(65);
-
 		Scanner scanner = new Scanner(source);
 		List<Token> tokens = scanner.scanTokens();
+		Parser parser = new Parser(tokens);
+		Expr expression = parser.parse();
 
-		for (Token token : tokens) {
-			System.out.println(token);
-		}
+		if (hadError) return;
+
+		interpreter.interpret(expression);
 	}
 
 	static void error(int line, String message) {
 		report(line, "", message);
 	}
+
+	static void error(Token token, String message) {
+		if (token.type == TokenType.EOF) {
+			report(token.line, " at end", message);
+		} else {
+			report(token.line, " at '" + token.lexeme + "'", message);
+		}
+	}
+
 
 	private static void report(int line, String where, String message) {
 		System.err.println("[line " + line + "] Error" + where + ": " + message);
@@ -62,4 +79,9 @@ public class Lox {
 		}
 	}
 
+
+	static void runtimeError(RuntimeError error) {
+		System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+		hadRuntimeError = true;
+	}
 }
